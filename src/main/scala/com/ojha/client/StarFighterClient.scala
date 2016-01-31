6b085ac39314645d837b56cf3436bbc4d2420fd6
polyglotpiglet <alexandra.p.batey@gmail.com>
2016-01-31 17:44:22 +0000

    little whitespace refactor

--- src/main/scala/com/ojha/client/StarFighterClient.scala
+++ src/main/scala/com/ojha/client/StarFighterClient.scala
@@ -12,26 +12,25 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object StarFighterClient extends Configurable {
   def apply(): StarFighterClient = {
-    val baseurl = config.getString("starfighter.baseurl")
-    val apikey = config.getString("starfighter.apikey")
-    new StarFighterClient(baseurl, apikey)
+    new StarFighterClient(config.getString("starfighter.baseurl"),
+                          config.getString("starfighter.apikey"))
   }
 }
 
-class StarFighterClient(baseurl: String, apikey: String) extends LazyLogging {
+class StarFighterClient(baseUrl: String, apiKey: String) extends LazyLogging {
 
-  logger.info(s"Started StarFighterClient with baseurl $baseurl")
+  logger.info(s"Started StarFighterClient with baseurl $baseUrl")
 
   def heartBeat(): Future[Heartbeat] = {
     import com.ojha.client.HeartbeatProtocol._
-    val request = url(s"$baseurl/heartbeat")
-    logger.info(s"Heartbeating $baseurl/heartbeat")
+    val request = url(s"$baseUrl/heartbeat")
+    logger.info(s"Heartbeating $baseUrl/heartbeat")
     execute[Heartbeat](request)
   }
 
   def heartBeatVenue(venue: String): Future[VenueHeartbeatResponse] = {
     import com.ojha.client.VenueHeartbeatResponseProtocol._
-    val urlPath = s"$baseurl/venues/$venue/heartbeat"
+    val urlPath = s"$baseUrl/venues/$venue/heartbeat"
     val request = url(urlPath)
     logger.info(s"Heartbeating venue $urlPath")
     execute[VenueHeartbeatResponse](request)
@@ -39,16 +38,16 @@ class StarFighterClient(baseurl: String, apikey: String) extends LazyLogging {
 
   def stocksOnAVenue(venue: String): Future[StocksInfoResponse] = {
     import com.ojha.client.StocksInfoResponseProtocol._
-    val urlPath = s"$baseurl/venues/$venue/stocks"
-    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apikey)
+    val urlPath = s"$baseUrl/venues/$venue/stocks"
+    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apiKey)
     logger.info(s"Checking stocks on venue $urlPath")
     execute[StocksInfoResponse](request)
   }
 
   def orderBookForStockOnVenue(venue: String, stock: String): Future[OrderBookResponse] = {
     import com.ojha.client.OrderBookResponseProtocol._
-    val urlPath = s"$baseurl/venues/$venue/stocks/$stock"
-    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apikey)
+    val urlPath = s"$baseUrl/venues/$venue/stocks/$stock"
+    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apiKey)
     logger.info(s"Checking orderbook for stock on venue $urlPath")
     execute[OrderBookResponse](request)
   }
@@ -57,54 +56,51 @@ class StarFighterClient(baseurl: String, apikey: String) extends LazyLogging {
     import com.ojha.client.NewOrderProtocol._
     import NewOrderResponseProtocol._
     val orderJson = order.toJson.compactPrint
-    val urlPath = s"$baseurl/venues/$venue/stocks/$stock/orders"
-    val request = url(urlPath).setBody(orderJson).setHeader("X-Starfighter-Authorization", apikey).POST
+    val urlPath = s"$baseUrl/venues/$venue/stocks/$stock/orders"
+    val request = url(urlPath).setBody(orderJson).setHeader("X-Starfighter-Authorization", apiKey).POST
     logger.info(s"Posting an order for stock at url: $urlPath, order:$orderJson ")
     execute[NewOrderResponse](request)
   }
 
   def getQuoteForStock(venue: String, stock: String): Future[StockQuoteResponse] = {
     import StockQuoteResponseProtocol._
-    val urlPath = s"$baseurl/venues/$venue/stocks/$stock/quote"
-    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apikey)
+    val urlPath = s"$baseUrl/venues/$venue/stocks/$stock/quote"
+    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apiKey)
     logger.info(s"Getting quote for stock $urlPath")
     execute[StockQuoteResponse](request)
   }
 
   def getStatusForOrder(venue: String, stock: String, id: Int): Future[OrderStatusResponse]= {
     import OrderStatusResponseProtocol._
-    val urlPath = s"$baseurl/venues/$venue/stocks/$stock/orders/$id"
-    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apikey)
+    val urlPath = s"$baseUrl/venues/$venue/stocks/$stock/orders/$id"
+    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apiKey)
     logger.info(s"Status for order $urlPath")
     execute[OrderStatusResponse](request)
   }
 
   def cancelOrder(venue: String, stock: String, id: Int): Future[OrderStatusResponse] = {
     import OrderStatusResponseProtocol._
-    val urlPath = s"$baseurl/venues/$venue/stocks/$stock/orders/$id"
-    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apikey).DELETE
+    val urlPath = s"$baseUrl/venues/$venue/stocks/$stock/orders/$id"
+    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apiKey).DELETE
     logger.info(s"Cancel order $urlPath")
     execute[OrderStatusResponse](request)
   }
 
   def getAllOrderStatuses(venue: String, account: String): Future[AllOrderStatusesResponse] = {
     import AllOrderStatusResponseProtocol._
-    val urlPath = s"$baseurl/venues/$venue/accounts/$account/orders"
-    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apikey)
+    val urlPath = s"$baseUrl/venues/$venue/accounts/$account/orders"
+    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apiKey)
     logger.info(s"Getting all orders on venue for an account $urlPath")
     execute[AllOrderStatusesResponse](request)
-
   }
-
 
   def getAllOrderStatusesForStock(venue: String, stock: String, account: String): Future[AllOrderStatusesResponse] = {
     import AllOrderStatusResponseProtocol._
-    val urlPath = s"$baseurl/venues/$venue/accounts/$account/stocks/$stock/orders"
-    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apikey)
+    val urlPath = s"$baseUrl/venues/$venue/accounts/$account/stocks/$stock/orders"
+    val request = url(urlPath).setHeader("X-Starfighter-Authorization", apiKey)
     logger.info(s"Getting all orders on venue for a stock/account $urlPath")
     execute[AllOrderStatusesResponse](request)
   }
-
 
   private def execute[T](request: Req)(implicit format: JsonFormat[T]): Future[T] = {
     val result = Http(request)
@@ -115,7 +111,4 @@ class StarFighterClient(baseurl: String, apikey: String) extends LazyLogging {
     logger.info("Shutting down StarFighterClient")
     dispatch.Http.shutdown()
   }
-
-
-
 }
